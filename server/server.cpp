191670813c011be06c72e1974d69fabb61b6a032
slplsswkds <slplsswkds@protonmail.com>
2024-09-28 01:37:04 +0300

    basic iterative server with greeting message

--- server/server.cpp
+++ server/server.cpp
@@ -1,3 +1,25 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+#define PORT 4666
+
 int main() {
+    try {
+        boost::asio::io_context io_context;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+
+        // iterative server
+        while (true) {
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
+            std::string message = "Hello from iterative server";
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        }
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
